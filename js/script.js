d4761467e23971b1ae2c87de57ce432b93b5cf2d
Dmytrosat <dmytrosat@gmail.com>
2025-11-09 11:13:57 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const contents = document.querySelectorAll(".program-line__content");
+
+contents.forEach((elem) => {
+  const title = elem.querySelector(".program-line__title");
+  const descr = elem.querySelector(".program-line__descr");
+
+  title.addEventListener("click", () => {
+    // Close all other descriptions
+    contents.forEach((item) => {
+      const itemDescr = item.querySelector(".program-line__descr");
+      if (itemDescr !== descr) {
+        itemDescr.classList.remove("active");
+      }
+    });
+
+    // Toggle the current description
+    descr.classList.toggle("active");
+  });
+});
